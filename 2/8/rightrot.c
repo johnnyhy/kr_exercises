953c87e014476107683904b6fdd5212d63d7ce90
Johnathan Yang <johnnyhy@umich.edu>
2019-07-18 08:35:10 -0400

    right rotate takes an unsigned int and shifts all bits right by n, wrapping around the front

---
+++ 2/8/rightrot.c
@@ -0,0 +1,26 @@
+/* Write a function rightrot(x, n) that returns the value
+ * of the integer x rotated to the right by n positions.
+ */
+
+#include <stdio.h>
+
+unsigned int rightrot(unsigned int x, int n);
+
+int main() {
+    int x = rightrot(8, 1);
+    printf("%d\n", x);
+}
+
+unsigned int rightrot(unsigned int x, int n) {
+    unsigned int maxbits = 8 * sizeof(unsigned int);
+    n %= maxbits;
+    if (n == 0) {
+        return x;
+    }
+
+    unsigned int front_wrap = x << (maxbits - n);
+    unsigned int back_wrap = (x >> n);
+    unsigned int back_mask = (1 << (maxbits - n)) - 1; // to account for undefined right shift behavior;
+    unsigned int result = front_wrap | (back_wrap & back_mask);
+    return result;
+}
